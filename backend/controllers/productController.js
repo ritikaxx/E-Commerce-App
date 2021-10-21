8facbfd8d18af8582b4ab808e6e7df3dc7224f42
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2021-10-21 13:18:00 +0530

    update and delete route added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -20,3 +20,50 @@ exports.getAllProducts = async(req,res)=>{
         product
     })
 }
+
+// update product --ADMIn
+
+exports.updateProduct = async(req,res,next)=>{
+
+    let product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false
+    });
+
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
+
+
+//delete product
+
+exports.deleteProduct = async(req,res,next)=>{
+
+    const product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success:true,
+            message:"product not found"
+        })
+
+    }
+
+    await product.remove();
+
+    res.status(200).json({
+        success:true,
+        message:"product deleted successfully"
+    })
+}
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -14,5 +14,7 @@ const {
 
 router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
+router.route("/product/:id").put(updateProduct);
+router.route("/product/:id").delete(deleteProduct);
 
 module.exports = router;

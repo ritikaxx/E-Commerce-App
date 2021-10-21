7b15da22c74de50f2c336a28359695a27346294c
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2021-10-21 15:36:44 +0530

    get product deatils routs added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -21,6 +21,24 @@ exports.getAllProducts = async(req,res)=>{
     })
 }
 
+// get product details
+exports.getProductDetails = async(req,res,next)=>{
+
+    const product = await Product.findById(req.params.id);
+
+if(!product){
+    return res.status(500).json({
+        success:false,
+        message:"Product not found"
+    })
+}
+
+res.status(200).json({
+    success:true,
+    product
+})
+}
+
 // update product --ADMIn
 
 exports.updateProduct = async(req,res,next)=>{
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -16,5 +16,6 @@ router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
 router.route("/product/:id").put(updateProduct);
 router.route("/product/:id").delete(deleteProduct);
+router.route("/product/:id").get(getProductDetails);
 
 module.exports = router;

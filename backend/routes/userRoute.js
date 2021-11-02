4c3dac63e9a0e542d7c88a3b08ef6ce84cab5de9
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2021-11-02 20:26:55 +0530

    update role and delete user

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -9,7 +9,9 @@ const {registerUser,
     updatePassword,
     updateProfile,
     getAllUser,
-    getSingleUser} = require("../controllers/userController");
+    getSingleUser,
+    updateUserRole,
+    deleteUser} = require("../controllers/userController");
 
 const {authorizeRoles,isAuthenticatedUser} = require("../middleware/auth");
 
@@ -23,7 +25,11 @@ router.route("/me").get(isAuthenticatedUser,getUserDetails);
 router.route("/password/update").put(isAuthenticatedUser,updatePassword);
 router.route("/me/update").put(isAuthenticatedUser,updateProfile);
 router.route("/admin/users").get(isAuthenticatedUser,authorizeRoles("admin"),getAllUser);
-router.route("/admin/user/:id").get(isAuthenticatedUser,authorizeRoles("admin"),getSingleUser);
+
+router.route("/admin/user/:id")
+    .get(isAuthenticatedUser,authorizeRoles("admin"),getSingleUser)
+    .put(isAuthenticatedUser,authorizeRoles("admin"),updateUserRole)
+    .delete(isAuthenticatedUser,authorizeRoles("admin"),deleteUser);
 
 
 module.exports = router;

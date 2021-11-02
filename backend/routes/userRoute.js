7c3a5790a3b9085dc6139ec755dd1a06cdd70aaf
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2021-11-02 11:59:28 +0530

    change password

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const {registerUser, loginUser, logout, forgotPassword,resetPassword} = require("../controllers/userController");
+const {registerUser,
+    loginUser,
+    logout,
+    forgotPassword,
+    resetPassword,
+    getUserDetails,
+    updatePassword} = require("../controllers/userController");
 
+const {authorizeRoles,isAuthenticatedUser} = require("../middleware/auth");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
-
 router.route("/logout").post(logout);
+
+router.route("/me").get(isAuthenticatedUser,getUserDetails);
+router.route("/password/update").put(isAuthenticatedUser,updatePassword);
+
 
 module.exports = router;

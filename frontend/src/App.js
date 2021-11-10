991a0e71fa9e8cbee6b51f92640630b24b99839b
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2021-11-10 19:32:58 +0530

    web font loader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
 import Header from "./component/layout/header.js";
 import {BrowserRouter as Router} from "react-router-dom";
+import WebFont from "webfontloader";
+import React from "react";
 
 function App() {
+  React.useEffect(()=>{
+    WebFont.load({
+      google:{
+        families:["Roboto" , "Droid Sans" , "Chilnka"],
+      },
+    });
+  }, [] );
   return (
      <Router>
        <Header />

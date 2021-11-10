52fc3b1c11436545d20f18ebf2abdc6db44edd18
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2021-11-10 19:23:29 +0530

    header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Header from "./component/layout/header.js";
+import {BrowserRouter as Router} from "react-router-dom";
 
 function App() {
   return (
-   
+     <Router>
+       <Header />
+     </Router>
   );
 }
 

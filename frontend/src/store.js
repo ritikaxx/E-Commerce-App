5a8ee4cd58b0ca12af777a13cb58e68a2cb984e7
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2021-11-12 00:26:52 +0530

    redux store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const reducer = combineReducers();
+const reducer = combineReducers({});
 let initialState=[];
 const middleware = [thunk];
 

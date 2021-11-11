faa44fe1fd47b2e7c2b3e8912c51d409c985d00c
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2021-11-11 20:33:38 +0530

    footer css

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,7 +1,15 @@
 import React, {Fragment} from 'react';
 import { CgMouse } from "react-icons/all";
 import "./Home.css";
+import Product from "./Product.js";
 
+const product = {
+  name: "Blue Jeans",
+  images: [{ url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fin.pinterest.com%2Frajdaniel61%2Fnavy-blue-shirt%2F&psig=AOvVaw2hzgNNl-k-vQTBixrp43Z1&ust=1636723338420000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNjbk92zkPQCFQAAAAAdAAAAABAJ"}],
+  price: "$300",
+  _id: "abhsihek",
+
+}
 const Home = () => {
     return (<Fragment>
         
@@ -15,6 +23,14 @@ const Home = () => {
               </button>
             </a>
           </div>
+
+          <h2 className="homeHeading">Featured Products</h2>
+          <div className="container" id="container">
+            <Product product={product}/>
+          </div>
+
+
+
     </Fragment>
     );
 };
---
+++ frontend/src/component/Home/Product.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Link} from "react-router-dom"; 
+
+const Product = () => {
+    return <div></div>;
+}
+
+export default Product;

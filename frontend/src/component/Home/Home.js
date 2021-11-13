e00d92647936c0df6961a424105800ce71132fac
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2021-11-13 17:08:34 +0530

    loader

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,10 +1,11 @@
 import React, {Fragment, useEffect} from 'react';
 import { CgMouse } from "react-icons/all";
 import "./Home.css";
-import Product from "./ProductCard.js";
+import Product from "./Product.js";
 import MetaData from "../layout/MetaData";
 import { getProduct } from "../../actions/productActions";
 import { useSelector, useDispatch } from 'react-redux';
+import Loader from "../layout/loader/loader";
 
 
 const Home = () => {
@@ -16,31 +17,37 @@ const Home = () => {
     dispatch(getProduct());
   }, [dispatch]);
     return (
-    <Fragment>
-        <MetaData title="ECOMMERCE" />
+   <Fragment>
+     { loading ? (
+       <Loader />
+     ) : (
+      <Fragment>
+      <MetaData title="ECOMMERCE" />
 
-        <div className="banner">
-            <p>Welcome to Ecommerce</p>
-            <h1>FIND AMAZING PRODUCTS BELOW</h1>
+      <div className="banner">
+          <p>Welcome to Ecommerce</p>
+          <h1>FIND AMAZING PRODUCTS BELOW</h1>
 
-            <a href="#container">
-              <button>
-                Scroll <CgMouse />
-              </button>
-            </a>
+          <a href="#container">
+            <button>
+              Scroll <CgMouse />
+            </button>
+          </a>
+      </div>
+
+        <h2 className="homeHeading">Featured Products</h2>
+        <div className="container" id="container">
+        {products &&
+            products.map((product) => 
+             <Product product = {product} />
+            )}
         </div>
 
-          <h2 className="homeHeading">Featured Products</h2>
-          <div className="container" id="container">
-          {products &&
-              products.map((product) => 
-               <Product product = {product} />
-              )}
-          </div>
 
 
-
-    </Fragment>
+  </Fragment>
+     )}
+   </Fragment>  
     );
 };
 
--- frontend/src/component/Home/Product.js
+++ frontend/src/component/Home/Product.js
@@ -2,15 +2,15 @@ import React from 'react';
 import {Link} from "react-router-dom"; 
 import ReactStars from "react-rating-stars-component";
 
-const options = {
+const Product = ({ product}) => {
+  const options = {
     edit: false,
     color: "rgba(20,20,20,0.1)",
     activeColor: "tomato",
     size: window.innerWidth <600 ? 20 : 25,
-    value: 2.5,
+    value: product.ratings,
     isHalf: true,
 };
-const Product = ({ product}) => {
     return (
         <Link className="productCard" to={`/product/${product._id}`}>
       <img src={product.images[0].url} alt={product.name} />
@@ -18,10 +18,10 @@ const Product = ({ product}) => {
       <div>
         <ReactStars {...options} />
         <span >
-          (256 reviews)
+          ({product.numOfReviews} Reviews)
         </span>
       </div>
-      <span>{product.price}</span>
+      <span>{`$ ${product.price}`}</span>
     </Link> 
     );
 };

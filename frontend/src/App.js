f7beb42ccf0ca49d8375f4e647786cb2026c804b
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2021-11-15 23:44:23 +0530

    search option added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import React from "react";
 import Home from "./component/Home/Home.js";
 import ProductDetails from './component/Product/ProductDetails';
 import Products from './component/Product/Products';
+import Search from "./component/Product/Search.js";
 
 function App() {
   React.useEffect(()=>{
@@ -26,6 +27,7 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/product/:id" element={<ProductDetails />} />
           <Route exact path="/products" element={<Products />} />
+          <Route exact path="/search" element={<Search />} />
 
 
       </Routes>

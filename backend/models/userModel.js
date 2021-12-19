b392641ef460922b89a40b6f43fadf083eed9277
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2021-12-19 14:54:34 +0530

    middlewares

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
-
-
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -47,40 +45,41 @@ const userSchema = new mongoose.Schema({
   resetPasswordToken: String,
   resetPasswordExpire: Date,
 });
-// hash of password
-userSchema.pre("save", async function(next){
 
-  if(!this.isModified("password")){
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
     next();
   }
 
-  this.password = await bcrypt.hash(this.password,10);
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
-//JWT TOKEN
-userSchema.methods.getJWTToken = function (){
-  return jwt.sign({id:this._id},process.env.JWT_SECRET, {
+// JWT TOKEN
+userSchema.methods.getJWTToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE,
   });
 };
 
-//password checking
-userSchema.methods.comparePassword = async function(enteredPassword){
-  return await bcrypt.compare(enteredPassword,this.password);
-}
+// Compare Password
 
-//generating password reset token
-userSchema.methods.getResetPasswordToken = function(){
-  //generating token
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// Generating Password Reset Token
+userSchema.methods.getResetPasswordToken = function () {
+  // Generating Token
   const resetToken = crypto.randomBytes(20).toString("hex");
 
-  //hashing and adding resetPasswordToken to userSchema
+  // Hashing and adding resetPasswordToken to userSchema
   this.resetPasswordToken = crypto
     .createHash("sha256")
     .update(resetToken)
     .digest("hex");
 
-  this.resetPasswordExpire = Date.now() +15 * 60 * 1000; 
+  this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+
   return resetToken;
 };
 
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,18 +1,22 @@
 const express = require("express");
-const { 
-    newOrder, 
-    getSingleOrder, 
-    myOrder, 
-    getAllOrders, 
-    updateOrder, 
-    deleteOrder 
+const {
+  newOrder,
+  getSingleOrder,
+  myOrders,
+  getAllOrders,
+  updateOrder,
+  deleteOrder,
 } = require("../controllers/orderController");
 const router = express.Router();
-const { isAuthenticatedUser, authorizeRoles} = require("../middleware/auth");
 
-router.route("/order/new").post(isAuthenticatedUser,newOrder);
-router.route("/order/:id").get(isAuthenticatedUser,getSingleOrder);
-router.route("/orders/me").get(isAuthenticatedUser,myOrder);
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+
+router.route("/order/new").post(isAuthenticatedUser, newOrder);
+
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
+
+router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+
 router
   .route("/admin/orders")
   .get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
